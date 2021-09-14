05c69aa820102f241bee1b7eecfbfc18f3de2c7c
wangli <wangli@geoway.com.cn>
2021-09-14 16:35:44 +0800

    初始化项目

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,25 @@
-const nav = require('./config/nav.js');
-const sidebar = require('./config/sidebar.js');
+const nav = require("./config/nav.js");
+const sidebar = require("./config/sidebar.js");
 
 module.exports = {
-    title: "vuepress-simple-demo 快速搭建说明文档、知识库",
-    description: "vuepress-simple-demo 快速搭建说明文档、知识库",
-    dest: 'dist',
-    // 如果不想将文档的小标题移到右侧，请去掉plugins中的vuepress-theme-vdoing，注释掉theme: "vdoing",
-    plugins: ["demo-container", "vuepress-theme-vdoing"],
-    theme: "vdoing",
-    base: "/",
-    themeConfig: {
-        logo: "https://vuejs.org/images/logo.png",
-        // 最近更新栏
-        updateBar: {
-            showToArticle: false, // 显示到文章页底部，默认true
-        },
-        sidebar: 'structuring',
-        category: false,
-        tag: false,
-        archive: false,
-        nav,
-        sidebar
+  title: "vuepress-simple-demo 快速搭建说明文档、知识库",
+  description: "vuepress-simple-demo 快速搭建说明文档、知识库",
+  dest: "./docs/dist",
+  // 如果不想将文档的小标题移到右侧，请去掉plugins中的vuepress-theme-vdoing，注释掉theme: "vdoing",
+  plugins: ["demo-container", "vuepress-theme-vdoing"],
+  theme: "vdoing",
+  base: "/",
+  themeConfig: {
+    logo: "https://vuejs.org/images/logo.png",
+    // 最近更新栏
+    updateBar: {
+      showToArticle: false, // 显示到文章页底部，默认true
     },
+    sidebar: "structuring",
+    category: false,
+    tag: false,
+    archive: false,
+    nav,
+    sidebar,
+  },
 };
